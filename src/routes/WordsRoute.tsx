70cb9aa6c4eeebbe1a2533863b10943ba23dfcad
Camilo Mejia <camilosw@gmail.com>
2020-05-26 08:05:38 +0200

    Random and alphabetical sorting

--- src/routes/WordsRoute.tsx
+++ src/routes/WordsRoute.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from 'astroturf';
 import Word from 'components/Word';
 import { WordsState } from './MainRoute';
@@ -15,6 +15,23 @@ const cn = css`
       text-decoration: underline;
     }
   }
+  .sort {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 1rem;
+  }
+  .sortAction {
+    margin-left: 0.25rem;
+    margin-right: 0.25rem;
+    cursor: pointer;
+    &.active,
+    &:hover {
+      color: var(--primary);
+    }
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 interface Props {
@@ -24,14 +41,45 @@ interface Props {
 }
 
 const WordsRoute = ({ type = 'unknown', words, toggleWord }: Props) => {
-  const { new: newWords, known: knownWords } = words;
+  const [sortRandom, setSortRandom] = useState(false);
+  const {
+    new: [...newWords],
+    known: [...knownWords],
+  } = words;
 
   const currentWords = type === 'known' ? knownWords : newWords;
+  if (type === 'unknown' && sortRandom) {
+    currentWords.sort(() => Math.random() - 0.5);
+  } else {
+    currentWords.sort();
+  }
 
   return (
     <div>
       <h1 className={cn.title}>{type === 'known' ? 'Known ' : 'New '}words</h1>
-      {[...currentWords].map(word => (
+      {type === 'unknown' && (
+        <div className={cn.sort}>
+          sort:
+          <span
+            onClick={() => setSortRandom(false)}
+            className={[cn.sortAction, !sortRandom && cn.active]
+              .filter(Boolean)
+              .join(' ')}
+          >
+            alphabetical
+          </span>{' '}
+          |{' '}
+          <span
+            onClick={() => setSortRandom(true)}
+            className={[cn.sortAction, sortRandom && cn.active]
+              .filter(Boolean)
+              .join(' ')}
+          >
+            random
+          </span>
+        </div>
+      )}
+      {currentWords.map(word => (
         <Word
           key={word}
           word={word}
